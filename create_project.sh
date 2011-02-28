159e96ce37b82beeb19d35aacb01dd5808f9001b
Leonardo Varuzza <varuzza@gmail.com>
2011-02-28 13:56:14 -0300

    Add pom and create project script

---
+++ create_project.sh
@@ -0,0 +1,6 @@
+mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-3:create \
+     -DarchetypeGroupId=org.scala-tools.archetypes \
+     -DarchetypeArtifactId=scala-archetype-simple \
+     -DremoteRepositories=http://scala-tools.org/repo-releases \
+     -DarchetypeVersion=1.1 \
+     -DgroupId=bioscala -DartifactId=bioscala
