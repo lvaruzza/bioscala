3fd4c6f02744bfea86447b3292daf728d27af705
Leonardo Varuzza <varuzza@gmail.com>
2011-03-01 15:36:59 -0300

    Read contigs in MappedReads

--- src/main/scala/bio/MappedReads.scala
+++ src/main/scala/bio/MappedReads.scala
@@ -10,7 +10,7 @@ import java.io.File
 package bio {
   object MappedReads {
 
-    def mappedReads(things: Iterator[Thing], seqs: ObjectContainer) = {
+    def mappedReads(things: Iterator[Thing], seqs: ObjectContainer,contigs: ObjectContainer) = {
 
       for (thing <- things) {
         if (thing.isInstanceOf[NR]) {
@@ -30,24 +30,31 @@ package bio {
       }
     }
 
+    def openAndImport(source:String,dbname:String):ObjectContainer = {
+    	val dbfile = new File(dbname)
+        if (dbfile.exists) dbfile.delete
+        val db = SeqDB.openDB(dbname)
+        dbfile.deleteOnExit        
+        SeqDB.importFasta(db, Source.fromFile(source));
+    	
+    	db
+    }
+    
     def main(args: Array[String]) {
       if (args.length > 0) {
         val graphFile = args(0) + "/LastGraph"
         val seqsFile = args(0) + "/Sequences"
+        val contigsFile = args(0) + "/contigs.fa"
 
         println("Graph file: " + graphFile)
         println("Seqs file: " + seqsFile)
         
         val (header, things) = readGraph(Source.fromFile(graphFile))
-
-        val dbname = "temp.db4o"
-        val dbfile = new File(dbname)
-        if (dbfile.exists) dbfile.delete
+        val seqdb = openAndImport(seqsFile,"seqs.db4o")
+        val contigsdb = openAndImport(contigsFile,"contigs.db4o")
         
-        val db = SeqDB.openDB(dbname)
-        SeqDB.importFasta(db, Source.fromFile(seqsFile));
-        mappedReads(things, db);
-        db.close
+        mappedReads(things, seqdb,contigsdb)
+        seqdb.close
       }
     }
   }
