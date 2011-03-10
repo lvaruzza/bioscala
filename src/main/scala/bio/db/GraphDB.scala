6abe14828b27d7e2253d5c89c25e946ab7cdd7d3
Leonardo Varuzza <varuzza@gmail.com>
2011-03-09 21:39:05 -0300

    Add code to import nodes

--- src/main/scala/bio/db/GraphDB.scala
+++ src/main/scala/bio/db/GraphDB.scala
@@ -1,16 +1,40 @@
 package bio.db
 
 import org.neo4j.kernel.EmbeddedGraphDatabase
-import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb._
+import bio.velvet.ReadGraph
+import scala.io.Source
+
+class MyRelationshipTypes extends Enumeration with RelationshipType
+{
+    val Link = Value
+}
 
 class GraphDB(db:GraphDatabaseService) {
-	
+	def importFile(filename:String) {
+		val (header,things) = ReadGraph.readGraph(Source.fromFile(filename))
+		val tx = db.beginTx();
+		
+		try {
+			for(thing <- things) {
+				if (thing.isInstanceOf[bio.velvet.Node]) {
+					val vnode = thing.asInstanceOf[bio.velvet.Node]
+					val node = db.createNode
+					node.setProperty("id", vnode.id)
+				}
+			}
+			tx.success
+
+		} finally {
+			tx.finish
+		}
+	}
 	def close { db.shutdown }
 }
 
 object GraphDB {
 	def openDB(filename:String) = {
-		val db:GraphDatabaseService = new EmbeddedGraphDatabase( filename );
+		val db:GraphDatabaseService = new EmbeddedGraphDatabase( filename );		
 		new GraphDB(db)
 	}
 }
